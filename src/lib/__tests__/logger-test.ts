0d3ec1c1682b993813011ea4f715043ff43a1e0a
Vincent Cheung <hello@mynameisvincent.com>
2019-01-23 11:50:19 -0800

    test(logger): unit tests for getting/setting loglevels/labels

--- src/lib/__tests__/logger-test.ts
+++ src/lib/__tests__/logger-test.ts
@@ -92,4 +92,36 @@ describe('Logger', () => {
       expect(mockfn.mock.calls[1][0].logLevel).toBe(2);
     });
   });
+
+  describe('getLogLevels', () => {
+    it ('returns the log levels for the logger', () => {
+      const levels = { foo: 1, bar: 2};
+      const sender = { send: function() {} };
+      const l = new Logger(sender, levels);
+      expect(l.getLogLevels()).toEqual(levels);
+    });
+  });
+
+  describe('setLabels', () => {
+    it ('applies the defined labels to all sent messages', () => {
+      const levels = { foo: 1};
+      const sender = { send: jest.fn()};
+      const l = new Logger(sender, levels);
+      l.setLabels({app: 'awesome'});
+      (l as any).foo('message');
+      expect(sender.send).toHaveBeenCalled();
+      expect(sender.send.mock.calls[0][0].labels['app']).toBe('awesome');
+    });
+  });
+
+  describe('getLabels', () => {
+    it ('returns the default labels on the logger', () => {
+      const levels = { foo: 1};
+      const sender = { send: function() {}};
+      const labels = { app: 'awesome' };
+      const l = new Logger(sender, levels);
+      l.setLabels(labels);
+      expect(l.getLabels()).toEqual(labels);
+    });
+  });
 });
